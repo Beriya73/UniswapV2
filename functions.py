b55f15c962a5ba51ede00ab3cf1e91f5ce6e53e2
rim30 <rim3000@gmail.com>
2024-12-19 11:28:10 +0200

    update2

--- functions.py
+++ functions.py
@@ -1,34 +1,48 @@
-from config import NETWORKS
+from config import NETWORKS, WETH_ADDRESS
 from termcolor import cprint, colored
 from web3 import Web3, HTTPProvider
 
-
-
 def get_network():
-    # Выбираем сеть
+    """
+    Выбирает сеть для выполнения свапа.
+
+    Возвращает:
+        tuple: Название выбранной сети и ее параметры из NETWORKS.
+    """
     keys = list(NETWORKS.keys())
     for key in enumerate(keys, 1):
         cprint(f'{key[0]}: {key[1]}', 'light_green')
     while True:
         try:
-            choice = int(input("Выберите сеть в которой будет сделан свап: "))
+            choice = int(input(colored("Выберите сеть в которой будет сделан свап: ", 'light_green')))
             if choice < 1 or choice > len(keys):
                 cprint("Некорректное число, повторите ввод!", 'light_yellow')
             else:
                 selected_network = keys[choice - 1]
-                cprint(f"Вы выбрали сеть {selected_network}")
+                cprint(f"Вы выбрали сеть {selected_network}", 'light_green')
                 return selected_network, NETWORKS[selected_network]
         except ValueError:
             cprint("Некорректный символ, введите число!", 'light_yellow')
 
+def get_token_addr(rpc_url: str, message) -> str:
+    """
+    Получает адрес токена, на который/с которого будет выполнен перевод.
 
-def get_out_token(rpc_url: str) -> str:
-    # Получаем адрес токена на который будем переводить
+    Параметры:
+        rpc_url (str): URL RPC для подключения к сети.
+
+    Возвращает:
+        str: Адрес токена.
+    """
+    cprint("0 - по умолчанию адрес ETH", 'light_yellow')
     w3 = Web3(HTTPProvider(rpc_url))
+
     while True:
-        token_address = input("Введите адрес токена на который будем переводить: ")
         # Проверка существования токена
         try:
+            token_address = input(colored(message, 'light_green'))
+            if token_address == '0':
+                return WETH_ADDRESS
             # Получение ABI токена (если известен)
             token_abi = w3.eth.get_code(token_address)
             if token_abi:
@@ -37,12 +51,20 @@ def get_out_token(rpc_url: str) -> str:
             else:
                 cprint(f"Токен с адресом {token_address} не существует", 'light_yellow')
                 continue
-        except Exception as e:
-            cprint(f"Ошибка при проверке токена: {e}", 'light_red')
+        except:
+            cprint(f"Ошибка при проверке токена!", 'light_red')
 
 
 def get_amount(balance: int) -> float:
-    # Получаем количество токена для вывода в wei
+    """
+    Получает количество токена для вывода в wei.
+
+    Параметры:
+        balance (int): Баланс в wei.
+
+    Возвращает:
+        float: Сумма перевода в нативном токене.
+    """
     bal_human = balance / (10 ** 18)
     cprint(f"На вашем счету: {bal_human} нативного токена", 'light_green')
     while True:
@@ -61,7 +83,13 @@ def get_amount(balance: int) -> float:
         except ValueError:
             cprint("Пожалуйста, введите корректное число.", 'light_red')
 
-def get_slippage()->float:
+def get_slippage() -> float:
+    """
+    Получает допустимый процент проскальзывания (Slippage).
+
+    Возвращает:
+        float: Процент проскальзывания.
+    """
     while True:
         try:
             slippage = float(input(colored("Введите допустимый процент проскальзывания (Slippage) в %: ",
@@ -75,6 +103,7 @@ def get_slippage()->float:
 
 if __name__ == '__main__':
     #get_network()
-    #check_outtoken('https://arbitrum.llamarpc.com')
+    get_token_addr('https://arbitrum.llamarpc.com',"Введите адрес токена с которого будем переводить:" )
     #get_amount(10000000000000000)
-    get_slippage()
+    #get_slippage()
+    #help(get_slippage)
--- uniswap2.py
+++ uniswap2.py
@@ -5,30 +5,35 @@ from dotenv import load_dotenv
 from termcolor import cprint
 import json
 from functions import (get_network,
-                       get_out_token,
+                       get_token_addr,
                        get_amount,
                        get_slippage)
-from config import WETH_ADDRESS
 from client import Client
-
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
 private_key = os.getenv("PRIVATE_KEY")
 proxy = os.getenv('proxy')
 
-
 with open('uniswap2_abi.json') as file:
     UNISWAP_V2_ROUTER_ABI = json.load(file)
 
 class UniswapV2Swap(Client):
+    """
+    Класс для выполнения свапов на платформе Uniswap V2 в сети Arbitrum.
+    """
     def __init__(self, private_key, proxy):
+        """
+        Инициализация класса UniswapV2Swap.
+
+        Параметры:
+            private_key (str): Приватный ключ кошелька.
+            proxy (str): Прокси для подключения к сети.
+        """
         super().__init__(private_key, proxy)
         self.chain_name, self.chain = get_network()
-        self.input_token = WETH_ADDRESS  # ETH
-        self.output_token = get_out_token(self.chain['rpc'])
-        #self.amount = get_amount(self.balance())
-        #self.slippage = slippage
+        self.input_token = get_token_addr(self.chain['rpc'], 'Введите адрес токена с которого будем переводить: ')
+        self.output_token = get_token_addr(self.chain['rpc'], 'Введите адрес токена на который будем переводить: ')
         self.w3 = AsyncWeb3(AsyncHTTPProvider(self.chain['rpc']))
         self.private_key = private_key
         try:
@@ -41,16 +46,37 @@ class UniswapV2Swap(Client):
         self.explorer_url = self.chain['explorer']
 
     async def balance(self):
+        """
+        Получает баланс кошелька.
+
+        Возвращает:
+            int: Баланс кошелька в wei.
+        """
         balance = await self.w3.eth.get_balance(self.address)
         return balance
 
-    async def get_amounts_out(self,amount):
-        self.path = [WETH_ADDRESS, self.output_token]
-        amounts = await self.router_contract.functions.getAmountsOut(amount, self.path).call()
+    async def get_amounts_out(self, amount):
+        """
+        Получает количество токенов, которые будут получены в результате свапа.
 
+        Параметры:
+            amount (int): Количество ETH для свапа.
+
+        Возвращает:
+            int: Количество токенов, которые будут получены.
+        """
+        self.path = [self.input_token, self.output_token]
+        amounts = await self.router_contract.functions.getAmountsOut(amount, self.path).call()
         return amounts[-1]
 
     async def exact_eth_for_tokens(self, slippage, amount):
+        """
+        Выполняет свап ETH на токены с учетом проскальзывания.
+
+        Параметры:
+            slippage (float): Допустимый процент проскальзывания.
+            amount (int): Количество ETH для свапа.
+        """
         amount_out_min = int(await self.get_amounts_out(amount) * (1 - slippage / 100))
         last_block = await self.w3.eth.get_block('latest')
         deadline = last_block.get('timestamp') + 300
@@ -64,10 +90,12 @@ class UniswapV2Swap(Client):
         await self.send_transaction(transaction=transaction)
 
 async def main():
+    """
+    Основная функция для выполнения свапа.
+    """
     swap = UniswapV2Swap(private_key, proxy)
-    amount = int(get_amount(await swap.balance())*10**18)
+    amount = int(get_amount(await swap.balance()) * 10**18)
     slippage = get_slippage()
     await swap.exact_eth_for_tokens(slippage, amount)
-
 
 asyncio.run(main())
